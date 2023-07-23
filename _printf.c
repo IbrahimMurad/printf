512ef04dc7d9da637235f75420801fd58cede9f2
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-23 12:09:42 +0300

    %% and %c

--- _printf.c
+++ _printf.c
@@ -15,11 +15,32 @@ int _printf(const char *format, ...)
 	int i = 0;
 	int printed = 0;
 	va_list args;
+	char temp_c;
 
 	va_start(args, format);
 	while (format[i])
 	{
-		printed += write(1, &format[i], 1);
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					temp_c = va_arg(args, int);
+					printed += write(1, &temp_c, 1);
+					break;
+				case '%':
+					temp_c = '%';
+					printed += write(1, &temp_c, 1);
+					break;
+				default:
+					return (-1); 
+			}
+		}
+		else
+		{
+			printed += write(1, &format[i], 1);
+		}
 		i++;
 	}
 	return printed;
